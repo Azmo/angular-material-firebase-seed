6104bf647f849b43c357801362d3ff245d8aef2d
Alexander Zhidkov <alexander.zhidkov@empired.com>
2017-08-19 18:22:32 +0800

    created seed project

---
+++ src/app/app.module.ts
@@ -0,0 +1,65 @@
+import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule, MdButtonModule, MdDialog, MdIconRegistry, MdInputModule, MdMenuModule, MdToolbarModule, MdTooltipModule } from '@angular/material';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuth, AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabase, AngularFireDatabaseModule } from 'angularfire2/database';
+import { AuthGuard } from './auth/auth-guard.service';
+
+import 'hammerjs';
+
+import { AppComponent } from './app.component';
+import { AppRouting } from './app.routing';
+import { LoginComponent } from './auth/login';
+import { FeedbackDialogComponent } from './feedback-dialog/feedback-dialog.component';
+import { HelpComponent } from './help/help.component';
+import { HomeComponent } from './home/home';
+import { PageNotFoundComponent } from './page.not.found';
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: 'XXX-D5GEN0RJGPKH0R4YrD-gPbk9wfGk-k',
+  authDomain: 'seed-dev.firebaseapp.com',
+  databaseURL: 'https://seed-dev.firebaseio.com',
+  projectId: 'seed-dev',
+  storageBucket: 'seed-dev.appspot.com',
+  messagingSenderId: '1005944715430',
+};
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HelpComponent,
+    HomeComponent,
+    FeedbackDialogComponent,
+    PageNotFoundComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRouting,
+    FormsModule,
+    FlexLayoutModule,
+    MaterialModule,
+    MdInputModule,
+    MdToolbarModule,
+    MdButtonModule,
+    MdMenuModule,
+    MdTooltipModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+  ],
+  providers: [
+    AuthGuard,
+    MdIconRegistry,
+    MdDialog,
+  ],
+  entryComponents: [FeedbackDialogComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
